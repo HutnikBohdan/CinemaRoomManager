69a8c495c573c6688c2e8374db07eb2eb340383c
bohdan <bohdanhutnik@gmail.com>
2024-05-12 18:02:10 +0200

    2/5 complete

--- src/Cinema.java
+++ src/Cinema.java
@@ -4,20 +4,17 @@ public class Cinema {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        char[][] cinema = new char[8][9];
-        for (int i = 0; i < cinema.length; i++) {
-            for (int j = 0; j < cinema[i].length; j++) {
-                if (i == 0 && j > 0) {
-                    cinema[0][j] = (char) (j + '0');
-                } else if (j == 0 && i > 0) {
-                    cinema[i][j] = (char) ((i) + '0');
-                } else if (i > 0 && j > 0) {
-                    cinema[i][j] = 'S';
-                } else {
-                    cinema[i][j] = ' ';
-                }
-            }
-        }
+/*        String[][] cinema = {
+                {" ", "1", "2", "3", "4", "5", "6", "7", "8"},
+                {"1", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"2", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"3", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"4", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"5", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"6", "S", "S", "S", "S", "S", "S", "S", "S"},
+                {"7", "S", "S", "S", "S", "S", "S", "S", "S"}
+        };
+
 
         System.out.println("Cinema:");
         for (int i = 0; i < cinema.length; i++) {
@@ -25,6 +22,42 @@ public class Cinema {
                 System.out.print(cinema[i][j] + " ");
             }
             System.out.println();
+        }*/
+        System.out.println("Enter the number of rows:");
+        int rows = input.nextInt();
+
+        System.out.println("Enter the number of seats in each row:");
+        int seatsInEachRow = input.nextInt();
+
+        int summ$ = 0;
+        if (rows * seatsInEachRow <= 60) {
+            summ$ = rows * seatsInEachRow * 10;
+        } else if (rows > 4) {
+            summ$ = ((rows / 2) * seatsInEachRow * 10) + (rows - (rows / 2))  * seatsInEachRow * 8;
         }
+
+        System.out.println("Total income:");
+        System.out.println("$" + summ$);
+
     }
 }
+
+
+
+
+
+
+/*
+        for (int i = 0; i < cinema.length; i++) {
+            for (int j = 0; j < cinema[i].length; j++) {
+                 if (i == 0 && j > 0) {
+                     cinema[0][j] =  (j + "");
+                 } else if (j == 0 && i > 0) {
+                     cinema[i][j] =(i + "");
+                 } else if (i > 0 && j > 0) {
+                    cinema[i][j] = "S";
+                 } else {
+                    cinema[i][j] = " ";
+                 }
+            }
+        }*/
